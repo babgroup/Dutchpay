365c94c23c8210b66eb0bcba10dc9e158bec7d4a
KangHuijun <khj50231@gmail.com>
2025-09-21 00:24:26 +0900

    feat(user): init User entity & app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['../.env'],
+    }),
+    RestaurantModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST'),
+        port: parseInt(config.get<string>('DB_PORT') ?? '3306', 10),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+        namingStrategy: new SnakeNamingStrategy(),
+    }),
+  }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
