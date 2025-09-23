ccbd08b1ee6098a06bf31315e90e78adf6a36411
Bacococc <pphj7353@naver.com>
2025-09-24 08:31:25 +0900

    🔥 remove : main.ts 파일 복원

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,18 @@ async function bootstrap() {
     .setTitle('DutchPay API')
     .setDescription('API documentation for DutchPay backend')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
