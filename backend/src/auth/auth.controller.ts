855e1b18bcb7cfbb23d1c7cf54f62d1113e845cd
KangHuijun <khj50231@gmail.com>
2025-09-23 21:47:26 +0900

    feat(auth): implement login, and JWT authentication

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Post, Body, HttpCode, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(200)
+  async login(@Body() loginDto: LoginDto ) {
+    const user = await this.authService.userAuth(loginDto);
+    return this.authService.login(user);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  getMe(@Req() req) {
+  return req.user; // DB 조회 필요 없으면 이 정도로 충분
+}
+}
