59a9496de127b8bf3df4197ca7a47d6e18596e95
seulzutora <seulzutora@gmail.com>
2025-09-24 21:57:40 +0900

    🎨 style: PartyTypeCard 수정

--- frontend/src/app/delivery/select-party-room/components/PartyTypeCard.tsx
+++ frontend/src/app/delivery/select-party-room/components/PartyTypeCard.tsx
@@ -14,7 +14,7 @@ export default function PartyTypeCard({
       className={`p-10 rounded-2xl border-2 text-center cursor-pointer transition-colors
         ${
           isSelected
-            ? 'bg-orange-50 border-orange-400 font-semibold text-orange-600'
+            ? 'bg-orange-50 border-orange-400 text-orange-600'
             : 'border-gray-200 text-gray-700 hover:border-gray-400'
         }`}
       onClick={onClick}
