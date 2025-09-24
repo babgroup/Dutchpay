303e0386656b805a2af90507d513252e58a4f52f
Bacococc <pphj7353@naver.com>
2025-09-24 20:27:26 +0900

    ✨ feat : 내 파티 확인 페이지(/leader)

---
+++ frontend/src/app/delivery/(withTopHeader)/leader/[id]/page.tsx
@@ -0,0 +1,20 @@
+import BasicButton from "@/app/components/BasicButton";
+import PartyInfoDiv from "../../components/PartyInfoDiv"
+import Link from "next/link";
+export const metadata = {
+  title: "소개 페이지",
+};
+
+export default function MyPartyPage() {
+  
+  return (
+    <>
+    <PartyInfoDiv />
+    <div className="justify-end w-full flex flex-col items-center">
+      <BasicButton text="메뉴 변경 요청하기"/>
+      <BasicButton text="계좌번호 공개하기"/>
+      <Link className="mt-2 text-gray-300" href="파티해산하기">파티 해산하기</Link> {/*추후 수정*/}
+    </div>
+    </>
+  )
+}
