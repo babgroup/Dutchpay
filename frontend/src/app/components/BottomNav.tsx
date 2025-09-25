4fa931886f0a17e8ae77ab6f91894bcdfacf9e49
Bacococc <pphj7353@naver.com>
2025-09-25 20:04:53 +0900

    ✏️ chore : BottomNav 로그인 여부(토큰 여부)에 따라 다른 경로로 라우팅

--- frontend/src/app/components/BottomNav.tsx
+++ frontend/src/app/components/BottomNav.tsx
@@ -1,25 +1,36 @@
 'use client';
 
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 interface BottomNavProps {
-  href: string;
+  href?: string;
   iconSrc: string;
   alt: string;
+  onClick?: () => void;
 }
 
-export default function BottomNav({ href, iconSrc, alt }: BottomNavProps) {
+export default function BottomNav({ href, iconSrc, alt, onClick }: BottomNavProps) {
   const pathname = usePathname();
   const isActive = pathname === href;
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault(); // Link 기본 이동 막기
+    if (onClick) {
+      onClick();
+    } else {
+      router.push(href); // onClick 이 없을 겅우 href로 이동
+    }
+  };
 
   return (
-    <Link href={href}>
-      <div className={`relative w-10 h-10 rounded-full flex items-center justify-center transition
-        ${isActive ? "bg-gray-200" : "bg-white"} hover:bg-gray-200`}>
-        <Image src={iconSrc} alt={alt} fill className="object-contain p-2.5" />
-      </div>
-    </Link>
+    <div
+      onClick={handleClick}
+      className={`relative w-10 h-10 rounded-full flex items-center justify-center transition
+        ${isActive ? "bg-gray-200" : "bg-white"} hover:bg-gray-200 cursor-pointer`}
+    >
+      <Image src={iconSrc} alt={alt} fill className="object-contain p-2.5" />
+    </div>
   );
 }
--- frontend/src/app/components/BottomNavLayout.tsx
+++ frontend/src/app/components/BottomNavLayout.tsx
@@ -1,11 +1,25 @@
+'use client';
+
 import BottomNav from "./BottomNav";
 
-export default function BottomNavLayout({ children }: { children?: React.ReactNode }) {
+export default function BottomNavLayout() {
   return (
     <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-1/4 h-12 bg-white rounded-3xl shadow-md flex items-center justify-around p-2">
       <BottomNav href="/" iconSrc="/home.svg" alt="home icon" />
       <BottomNav href="/delivery" iconSrc="/delivery.svg" alt="delivery icon" />
-      <BottomNav href="/user" iconSrc="/user.svg" alt="user icon" />
+      {/* 로그인 여부에 따라 /user 또는 /login 이동, 로컬 스토리지 */}
+      <BottomNav
+        iconSrc="/user.svg"
+        alt="user icon"
+        onClick={() => {
+          const token = localStorage.getItem("jwtToken");
+          if (token) {
+            window.location.href = `/user`;
+          } else {
+            window.location.href = "/login";
+          }
+        }}
+      />
     </div>
   );
 }
