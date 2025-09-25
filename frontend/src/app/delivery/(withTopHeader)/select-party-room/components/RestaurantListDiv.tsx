36cfe58e5dcd9a671960eedc9a7a9b5e7be36d07
seulzutora <seulzutora@gmail.com>
2025-09-25 22:09:09 +0900

    ✨ feat: made RestaurantListCard.tsx and RestaurantListDiv.tsx

---
+++ frontend/src/app/delivery/(withTopHeader)/select-party-room/components/RestaurantListDiv.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import RestaurantListCard from "./RestaurantListCard";
+import useFetch from "@/common/customFetch";
+import { RestaurantList } from "@/types/restaurant";
+
+export default function RestaurantListDiv() {
+    const Fetch = useFetch();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [list, setList] = useState<RestaurantList[]>([])
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        const fetchRestaurantList = async () => {
+            setLoading(true);
+            setMessage('로딩 중...');
+            try {
+                const response = await Fetch("restaurant/list", { method: "GET" })
+                console.log(response);
+                if (response && Array.isArray(response.data)) {
+                    setList(response.data);
+                } else {
+                    setList([]);
+                    setMessage('데이터를 불러오는 데 실패했습니다');
+                }
+            } catch (e: unknown) {
+                setMessage(e.message || '오류가 발생했습니다');
+                setList([]);
+            } finally {
+                setLoading(false);
+                setMessage('');
+            }
+        };
+        fetchRestaurantList();
+    }, []);
+
+    return (
+        <div className="flex flex-col gap-3">
+            {list.map((res) => (
+                <RestaurantListCard
+                    key={res.id} 
+                    id={res.id}
+                    restaurantName={res.restaurantName}
+                    deliveryFee={res.deliveryFee}
+                    imageUrl={res.imageUrl}
+                    businessHours={res.businessHours}
+                />
+            ))}
+        </div>
+    );
+}
--- frontend/src/app/delivery/(withTopHeader)/select-party-room/components/SelectRestaurant.tsx
+++ frontend/src/app/delivery/(withTopHeader)/select-party-room/components/SelectRestaurant.tsx
@@ -1,18 +1,16 @@
-import BasicButton from "@/app/components/BasicButton";
+'use client'
 
-interface SelectRestaurantProps {
-    onBack: () => void;
-    onNext: () => void;
-    selectedType: string | null;
-    selectedPartySize: string | null;
-}
+import BasicButton from "@/app/components/BasicButton";
+import { useEffect, useState } from "react";
+import RestaurantListDiv from "./RestaurantListDiv";
 
 export default function SelectRestaurant({
     onBack,
     onNext,
     selectedType,
-    selectedPartySize,
-}: SelectRestaurantProps) {
+    selectedPartySize
+}) {
+    const [selectedId, setSelectedId] = useState<string | null>(null);
     
     const getDescription = () => {
         if (selectedType === 'A') {
@@ -29,6 +27,10 @@ export default function SelectRestaurant({
             <div className="flex flex-col justify-center items-center m-4">
                 <p className="text-xl text-black">{getDescription()}</p>
                 <p className="text-xs text-gray-400 m-4">* 해당 목록은 영업시간에 따른 배달 가능 매장 목록입니다. 가게 사정 등으로 현재 배달이 불가능 한 곳이 있을 수 있으니 직접 확인 해주세요.</p>
+            </div>
+
+            <div>
+                <RestaurantListDiv />
             </div>
             
             <div className="flex flex-col m-10 items-center">
