a90b984445ac0ec494556be193e90f9b82f4f7fc
Bacococc <pphj7353@naver.com>
2025-09-25 20:44:16 +0900

    ✏️ chore : api 사용 토큰 유효성 검사 방식으로 수정

--- frontend/src/app/components/BottomNavLayout.tsx
+++ frontend/src/app/components/BottomNavLayout.tsx
@@ -1,9 +1,34 @@
 'use client';
 
 import BottomNav from "./BottomNav";
-import isTokenValid from "@/common/jwdTokenVaild";
+import useFetch from "@/common/customFetch";
+
 
 export default function BottomNavLayout() {
+  const Fetch = useFetch();
+
+  const handleUserClick = async () => {
+    const token = localStorage.getItem("jwtToken");
+    if (!token) {
+      window.location.href = "/login"; // 토큰이 없을 경우 로그인 페이지로 라우트
+      return;
+    }
+    try {
+      const res = await Fetch("/auth/me", {method: "GET"});
+
+      if (res.ok) {
+        const data = await res.json();
+        window.location.href = `/user/${data.id}`; // 응답 내 유저 id 사용
+      } else {
+        window.location.href = "/login"; 
+      }
+    } catch (error) {
+      window.location.href = "/login";
+    }
+  };
+
+
+
   return (
     <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 w-1/4 h-12 bg-white rounded-3xl shadow-md flex items-center justify-around p-2">
       <BottomNav href="/" iconSrc="/home.svg" alt="home icon" />
@@ -12,14 +37,7 @@ export default function BottomNavLayout() {
       <BottomNav
         iconSrc="/user.svg"
         alt="user icon"
-        onClick={() => {
-          const token = localStorage.getItem("jwtToken");
-          if (token && isTokenValid(token)) {
-            window.location.href = `/user`;
-          } else {
-            window.location.href = "/login";
-          }
-        }}
+        onClick={handleUserClick}
       />
     </div>
   );
