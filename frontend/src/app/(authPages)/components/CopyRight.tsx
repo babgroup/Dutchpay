c54ee6a6bb9203f7b91e48ce6b8fec97c03a7a42
Bacococc <pphj7353@naver.com>
2025-09-26 11:48:33 +0900

    ✏️ chore : 깃허브 이미지 -> 밥그룹 이미지로 변경

--- frontend/src/app/(authPages)/components/CopyRight.tsx
+++ frontend/src/app/(authPages)/components/CopyRight.tsx
@@ -3,7 +3,7 @@ export default function CopyRight() {
 
   return (
     <a className="flex flex-col items-center gap-2 text-gray-500 text-xs mt-auto mb-20">
-        <Image height={40} width={40} src="/git.png" alt="a github icon connected to github page" />
+        <Image height={40} width={40} src="/babgroup.webp" alt="a github icon connected to github page" />
       @2024 Team Bab group
     </a>
   )
