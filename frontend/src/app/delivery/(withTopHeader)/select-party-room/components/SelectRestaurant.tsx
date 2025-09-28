cdc11ca48fb41e17905a4018f315d3f600f3da86
seulzutora <seulzutora@gmail.com>
2025-09-28 16:14:24 +0900

    ✨ feat: add delivery fee per person

--- frontend/src/app/delivery/(withTopHeader)/select-party-room/components/SelectRestaurant.tsx
+++ frontend/src/app/delivery/(withTopHeader)/select-party-room/components/SelectRestaurant.tsx
@@ -23,7 +23,17 @@ export default function SelectRestaurant({
 }: SelectRestaurantProps) {
     const getDescription = () => {
         if (selectedParty?.type === 'A') {
-            return selectedParty.partySize ? `${selectedParty.partySize}명이 모일 경우...` :  '';
+            if (selectedParty.partySize && selectedRestaurant) {
+                const perPersonFee = Math.ceil(selectedRestaurant.deliveryFee / selectedParty.partySize);
+                return (
+                    <div>
+                        <div className="text-xl">
+                            {selectedParty.partySize}명이 모이면 1인당 <span className="text-orange-500">{perPersonFee}원</span>
+                        </div>
+                    </div>
+                )
+            }
+            return selectedParty.partySize ? `${selectedParty.partySize}명이 모이면...` :  '';
         }
         if (selectedParty?.type === 'B') {
             return '1/N의 배달비가 나옵니다.'
@@ -34,7 +44,7 @@ export default function SelectRestaurant({
     return (
         <div className="flex flex-col h-full p-6">
             <div className="flex flex-col justify-center items-center m-4">
-                <p className="text-xl text-black">{getDescription()}</p>
+                <div className="text-xl text-black">{getDescription()}</div>
                 <p className="text-xs text-gray-400 m-4">* 해당 목록은 영업시간에 따른 배달 가능 매장 목록입니다. 가게 사정 등으로 현재 배달이 불가능 한 곳이 있을 수 있으니 직접 확인 해주세요.</p>
             </div>
 
