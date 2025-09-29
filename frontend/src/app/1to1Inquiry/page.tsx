2f1f24f1ba2778f4d37ec3f1e2fd51280d115734
seulzutora <seulzutora@gmail.com>
2025-09-29 18:38:55 +0900

    Revert "✨ feat: add inquiry dropdown"
    This reverts commit c74231ec2556772c47bed48c69d77320e417a693.

--- frontend/src/app/1to1Inquiry/page.tsx
+++ frontend/src/app/1to1Inquiry/page.tsx
@@ -1,32 +1,5 @@
-'use client'
-
-import SelectDropdown, { DropdownOption } from "../delivery/(withTopHeader)/select-party-room/components/Dropdown"
-import { useState } from "react";
-
-
 export default function Inquiry() {
-    const inquiryOptions: DropdownOption[] = [
-        { value: '1', label: '계정/개인정보' },
-        { value: '2', label: '결제' },
-        { value: '3', label: '서비스 이용' },
-        { value: '4', label: '기기/환경' },
-        { value: '5', label: '제안/피드백' },
-        { value: '6', label: '기타' },
-    ]
-
-    const [selectedInquiry, setSelectedInquiry] = useState<string | null>(null);
-
     return (
-        <div className="flex flex-col h-full p-6">
-            <div className="mb-15" />
-
-            <SelectDropdown
-                label=" 카테고리 선택"
-                placeholder="문의 사항을 선택해주세요"
-                options={inquiryOptions}
-                selectedValue={selectedInquiry}
-                onSelect={setSelectedInquiry}
-            />
-        </div>
+        <h1>1 to 1 inquiry</h1>
     )
 }
