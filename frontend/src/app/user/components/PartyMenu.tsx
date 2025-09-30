d068af44e18214a3b126c1ebb2d4c9e688210428
Bacococc <pphj7353@naver.com>
2025-09-30 11:16:51 +0900

    ✏️ chore : 1:1문의 url 추가

--- frontend/src/app/user/components/PartyMenu.tsx
+++ frontend/src/app/user/components/PartyMenu.tsx
@@ -6,9 +6,9 @@ export default function PartyMenu() {
 
   return (
     <div className="flex flex-col w-4/5 m-4">
-      <Link href="/내계좌설정" className="text-black py-3 border-b border-t border-gray-200">내 계좌 설정</Link>
-      <Link href="/1:1문의" className="text-black py-3 border-b border-gray-200">1 : 1 문의</Link>
-      <Link href="/유저신고" className="text-black py-3 border-b border-gray-200">유저 신고</Link>
+      <Link href="/" className="text-black py-3 border-b border-t border-gray-200">내 계좌 설정</Link> {/* url 수정 필요 */}
+      <Link href="/1to1inquiry" className="text-black py-3 border-b border-gray-200">1 : 1 문의</Link>
+      <Link href="/" className="text-black py-3 border-b border-gray-200">유저 신고</Link> {/* url 수정 필요 */}
       <p
         className="block text-black py-3 border-b border-gray-200 cursor-pointer"
         onClick={() => {
