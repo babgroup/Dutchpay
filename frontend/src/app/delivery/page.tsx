411a8ff9d3f57ec248c6ac5eec4ee7e9d047f8b2
Bacococc <pphj7353@naver.com>
2025-10-02 15:38:34 +0900

    ✨ feat : 파티가 진행 중일 때 /delivery 페이지로 접속 시 파티 페이지로 라우팅 되도록

--- frontend/src/app/delivery/page.tsx
+++ frontend/src/app/delivery/page.tsx
@@ -1,9 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import RoomListDiv from "./components/RoomListDiv";
 import Title from "../components/Title";
 import PlusButton from "./components/PlusButton";
 import BottomNavLayout from "../components/BottomNavLayout";
+import useCustomFetch from "@/common/customFetch";
 
-export default async function Home() {
+export default function Home() {
+  const router = useRouter();
+  const apiFetch = useCustomFetch();
+  const [progress, setProgress] = useState();
+
+  useEffect(() => {
+    const roomId = localStorage.getItem("currentRoomId");
+    const fetchProgress = async () => { 
+      if (!roomId) return;
+      try {
+        const res = await apiFetch(`/restaurant/progress/${roomId}`, {method: 'GET'})
+        if(res.ok) {
+          setProgress(res.data)
+        }
+      } catch(error) {
+        if(error instanceof Error) console.log(error.message);
+      }
+    };
+
+    fetchProgress();
+  }, []);
+
+  useEffect(() => {
+    const roomId = localStorage.getItem("currentRoomId");
+    const role = localStorage.getItem("currentRole"); 
+    if (!roomId || !role || !progress) return;
+
+      if (progress !== "3" && progress !== "2") { // 2(주문완료) 혹은 3(파티 해산)이 되었으면 더이상 강제 라우팅 X
+        router.replace(`/delivery/${role}/${roomId}`);
+      }
+  }, [progress, router]);
 
   return (
     <div className="flex flex-col items-center w-full h-full overflow-auto pt-0 mb-20">
