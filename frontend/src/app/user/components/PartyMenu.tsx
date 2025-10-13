cfdd79f35e5cefd19958c19ea3af383442d98d27
seulzutora <seulzutora@gmail.com>
2025-10-12 18:06:30 -0700

    ✨ feat: update inquiry routing

--- frontend/src/app/user/components/PartyMenu.tsx
+++ frontend/src/app/user/components/PartyMenu.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import Link from "next/link"
+import { useParams } from "next/navigation";
 
 export default function PartyMenu() {
+  const params = useParams();
+  const id = (params && params.id) as string;
 
   return (
     <div className="flex flex-col w-4/5 m-4">
       <Link href="/" className="text-black py-3 border-b border-t border-gray-200">내 계좌 설정</Link> {/* url 수정 필요 */}
-      <Link href="/1to1Inquiry" className="text-black py-3 border-b border-gray-200">1 : 1 문의</Link>
+      <Link href={`/user/${id}/1to1Inquiry`} className="text-black py-3 border-b border-gray-200">1 : 1 문의</Link>
       <Link href="/" className="text-black py-3 border-b border-gray-200">유저 신고</Link> {/* url 수정 필요 */}
       <p
         className="block text-black py-3 border-b border-gray-200 cursor-pointer"
