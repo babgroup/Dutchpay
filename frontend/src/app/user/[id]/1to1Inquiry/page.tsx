e8872c9dc080ba98032f228a2c2319a5ebf7a614
seulzutora <seulzutora@gmail.com>
2025-10-13 01:25:10 -0700

    ✨ feat: send user inquiry via EmailJS

--- frontend/src/app/user/[id]/1to1Inquiry/page.tsx
+++ frontend/src/app/user/[id]/1to1Inquiry/page.tsx
@@ -1,9 +1,12 @@
-import InquiryForm from "./components/InquiryForm"
+import InquiryForm from "./components/InquiryForm";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function InquiryPage() {
     return (
         <div className="h-full">
             <InquiryForm />
+            <ToastContainer position="top-center" theme="colored" />
         </div>
     )
 }
