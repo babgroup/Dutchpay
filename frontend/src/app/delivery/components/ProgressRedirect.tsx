ae585de9f7984c3215c6b5f55f137e3cc88f420d
Bacococc <pphj7353@naver.com>
2025-10-26 23:44:15 +0900

    🐛 fix : 구조분해 오류 수정(res.data.data -> res.data)

--- frontend/src/app/delivery/components/ProgressRedirect.tsx
+++ frontend/src/app/delivery/components/ProgressRedirect.tsx
@@ -13,22 +13,24 @@ export default function ProgressRedirect() {
         let roomId = localStorage.getItem("currentRoomId");
         let role: string = localStorage.getItem("currentRole") ?? "";
 
+
         // 로컬에 정보 없으면 API로 조회
         if (!roomId || !role) {
           const res = await apiFetch("/restaurant/my-room");
-          const { inRoom, roomId: fetchedRoomId, role: fetchedRole } = res.data.data;
 
-          // 참여 중인 방이 없으면 코드 중지
-          if (!inRoom) return;
+          const { inRoom, roomId: fetchedRoomId, role: fetchedRole } = res.data;
+
+          if (!inRoom) return; // 참여 중인 방 없으면 종료
 
           // 가져온 값 로컬에 저장
           roomId = String(fetchedRoomId);
-          role = fetchedRole.toLowerCase(); // api 로 받아온 role 데이터를 항상 소문자로
+          if (fetchedRole === "CREATOR") {
+            role = "leader";
+          } else {
+            role = "member";
+          }
           localStorage.setItem("currentRoomId", roomId);
           localStorage.setItem("currentRole", role);
-        } else {
-          // 기존 저장된 role도 소문자 통일
-          role = role.toLowerCase();
         }
 
         // 진행 상태 조회
@@ -38,11 +40,9 @@ export default function ProgressRedirect() {
 
         // 끝난 상태(완료, 해산)일 때 로컬 스토리지에서 값 지우기
         if (progress === 2 || progress === 3) {
-          // 완료 or 해산 상태라면 로컬 데이터 삭제
           localStorage.removeItem("currentRoomId");
           localStorage.removeItem("currentRole");
         } else {
-          // 파티 끝나지 않았으면 해당 페이지로 이동
           router.replace(`/delivery/${role}/${roomId}`);
         }
       } catch (error) {
